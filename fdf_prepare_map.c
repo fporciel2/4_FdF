6c74dff28a02cbe3695ea2c789d2b1d950812316
fporciel <fporciel@student.42roma.it>
2023-11-21 11:31:36 +0100

    Retrying map's building.

--- fdf_prepare_map.c
+++ fdf_prepare_map.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 13:52:15 by fporciel          #+#    #+#             */
-/*   Updated: 2023/11/21 10:38:59 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:30:08 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -33,88 +33,8 @@
 
 int	fdf_free_map(t_fdf *fdf)
 {
-	ssize_t	imap;
-
-	imap = fdf->height;
-	while (imap != fdf->imap)
-	{
-		free(((fdf->map)[imap]));
-		((fdf->map)[imap]) = NULL;
-		imap--;
-	}
-	free(fdf->map);
-	fdf->map = NULL;
-	return (0);
-}
-
-static void	fdf_continue_fill_map(t_fdf *fdf, ssize_t j)
-{
-	ssize_t	i;
-
-	fdf->spline = ft_split(fdf->line, 32 );
-	if (fdf->spline == NULL)
-		fdf_generic_error(fdf);
-	i = 0;
-	while (((fdf->spline)[i]) != NULL)
-	{
-		((fdf->map)[j][i]) = ft_atoi(((fdf->spline)[i]));
-		i++;
-	}
-	fdf->spline = fdf_free_split(fdf->spline);
-}
-
-static void	fdf_fill_map(t_fdf *fdf, char *name)
-{
-	ssize_t	j;
-
-	j = 0;
-	fdf->stop = 1;
-	fdf->fd = open(name, O_RDONLY);
-	if (fdf->fd < 0)
-		fdf_generic_error(fdf);
-	while (fdf->stop != 0)
-	{
-		errno = 0;
-		fdf->line = get_next_line(fdf->fd);
-		if ((fdf->line == NULL) && ((errno == ENOMEM)
-				|| (errno == EAGAIN) || (errno == EWOULDBLOCK)
-				|| (errno == EBADF) || (errno == EFAULT) || (errno == EINTR)
-				|| (errno == EINVAL) || (errno == EIO)))
-			fdf_generic_error(fdf);
-		if (fdf->line == NULL)
-			fdf->stop = 0;
-		if (fdf->line != NULL)
-			fdf_continue_fill_map(fdf, j);
-		free(fdf->line);
-		j++;
-	}
-	if (close(fdf->fd) < 0)
-		fdf_generic_error(fdf);
 }
 
 int	**fdf_prepare_map(t_fdf *fdf, char *name)
 {
-	int	result;
-
-	fdf->map = (int **)malloc(sizeof(int *) * (fdf->height + 1));
-	if (fdf->map == NULL)
-	{
-		result = fdf_generic_error(fdf);
-		return (fdf->map);
-	}
-	((fdf->map)[fdf->height]) = NULL;
-	fdf->imap = fdf->height;
-	while (fdf->imap >= 0)
-	{
-		((fdf->map)[fdf->imap]) = (int *)malloc(sizeof(int) * (fdf->width + 1));
-		if (((fdf->map)[fdf->imap]) == NULL)
-		{
-			result = fdf_generic_error(fdf);
-			return (fdf->map);
-		}
-		(fdf->imap)--;
-	}
-	(void)result;
-	fdf_fill_map(fdf, name);
-	return (fdf->map);
 }
