0bdaa23a0f6c458fac27a5bef20982990bdd6822
fporciel <fporciel@student.42roma.it>
2023-12-01 09:53:14 +0100

    Some differences.

--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 15:04:10 by fporciel          #+#    #+#             */
-/*   Updated: 2023/11/30 20:29:33 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/12/01 09:50:31 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -139,5 +139,6 @@ int		fdf_get_vert_x1(t_fdf *fdf, int x, int y);
 int		fdf_get_vert_y1(t_fdf *fdf, int x, int y);
 int		fdf_get_orth_x(t_fdf *fdf, int x);
 int		fdf_get_orth_y(t_fdf *fdf, int y);
+char	**fdf_split(const char *str);
 
 #endif
--- fdf_prepare_map.c
+++ fdf_prepare_map.c
@@ -6,7 +6,7 @@
 /*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 13:52:15 by fporciel          #+#    #+#             */
-/*   Updated: 2023/11/30 20:30:05 by fporciel         ###   ########.fr       */
+/*   Updated: 2023/12/01 09:51:14 by fporciel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*
@@ -68,7 +68,7 @@ static int	*fdf_count_split_and_allocate(t_fdf *fdf)
 
 static int	fdf_split_line_and_fill(t_fdf *fdf, char *line, int result)
 {
-	fdf->spline = ft_split(line, 32);
+	fdf->spline = fdf_split(line);
 	if (fdf->spline == NULL)
 		result = fdf_generic_error(fdf);
 	fdf->i = 0;
